0d5be6a057b2404b73f0806f327826c3d3c0096c
dudinmaster <imaddudina@gmail.com>
2023-03-02 12:49:31 +0700

    penggunaan operator artimatik dalam looping

---
+++ tugas10/tugas10.js
@@ -0,0 +1,8 @@
+var kelipatan = 1
+for (var i = 1; i <= 55; i += kelipatan){
+    // for (var y=2 y)
+    console.log("ini adalah urutan ke :",i)
+    // console.log(kelipatan)
+    // kelipatan = i
+    kelipatan += 1
+}
