641836ff083c4925f3a6a7f6e93685537d061561
dudinmaster <imaddudina@gmail.com>
2023-03-02 11:45:20 +0700

    menmpilkaan isi object menggunakan for in

---
+++ tugas9/tugas9.js
@@ -0,0 +1,13 @@
+function panggilChar(){
+    var char1 = {
+        name : 'Al-haitam',
+        weapon : 'Lofi',
+        levelTalent : [9,10,7],
+    }
+    
+    for (var x in char1){
+        console.log(char1[x])
+    }
+}
+
+panggilChar()
